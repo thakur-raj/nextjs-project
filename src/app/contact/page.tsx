08352bfe4a1fb511e56277574605a2593386b817
Raj Thakur <raj_thakur@rocketmail.com>
2024-01-30 07:42:32 +0000

    Contact page designed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,40 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="contact" fill className={styles.contactImg}/>
+      </div>
+      <div className={styles.formContainer}>
+        <form className={styles.formTag}>
+          <input
+            type="text"
+            placeholder="Name and Surname"
+            className={styles.input}
+          />
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className={styles.input}
+          />
+          <input
+            type="text"
+            placeholder="Enter your Phone number (Optional)"
+            className={styles.input}
+          />
+          <textarea
+            placeholder="Enter your message"
+            className={styles.textarea}
+            cols={30}
+            rows={10}
+          />
+          <button className={styles.buttonElement}>Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
