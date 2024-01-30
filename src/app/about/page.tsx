e6de053d326129e1059477fa095036feed6aebc5
Raj Thakur <raj_thakur@rocketmail.com>
2024-01-30 07:12:28 +0000

    Aout page designed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,46 @@
-import Image from "next/image"
-import styles from "./about.module.css"
+import Image from "next/image";
+import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div className={styles.imageContainer}>
-      <Image src='/about.png' alt='about' fill/>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.subTitle}>About Agency</h1>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Projects Completed</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Projects Completed</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Projects Completed</p>
+          </div>
+        </div>
       </div>
-  )
-}
+      <div className={styles.imageContainer}>
+        <Image
+          src="/about.png"
+          alt="about"
+          fill
+          className={styles.aboutImg}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
